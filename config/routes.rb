db1b446c300771d0bc8d5177239c5e088ac1193e
mmazanec22 <melanie.mazanec@gmail.com>
2016-09-14 11:26:03 -0500

    Make all the pages and links.  No content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pages#bio'
+  root 'pages#bio', as: 'bio'
+
+  get '/blog', to: 'pages#blog', as: 'blog'
+
+  get '/dev_projects', to: 'pages#dev_projects', as: 'dev_projects'
+
+  get 'civic_projects', to: 'pages#civic_projects', as: 'civic_projects'
+
+  get '/stories', to: 'pages#stories', as: 'stories'
 
 end
