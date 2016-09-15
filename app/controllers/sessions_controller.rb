38996cd7ca10dae095f9e2d5a113150b48566afd
mmazanec22 <melanie.mazanec@gmail.com>
2016-09-14 21:06:53 -0500

    Start making login stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #if password param is the same as the environment variable that is my password, then go to create new post page
+    #otherwise, direct to my bio page with a silly error-- you aren't me, this is me. did you forget?
+  end
+
+  def destroy
+  end
+
+end
---
+++ db/migrate/20160915015853_create_posts.rb
@@ -0,0 +1,6 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+    end
+  end
+end
