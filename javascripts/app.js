4ed25995801fd5729aeca06e549265a5e4fbcf00
mmazanec22 <melanie.mazanec@gmail.com>
2017-08-12 08:49:27 -0500

    Use four spaces

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  $("p").hide()
-  $("p.bio").show()
-  $("li.link").on("click", function(){
     $("p").hide()
-    var toShow = $(this).attr("class").split(" ")[0]
-    $("p."+toShow).show()
-  })
+    $("p.bio").show()
+    $("li.link").on("click", function(){
+        $("p").hide()
+        var toShow = $(this).attr("class").split(" ")[0]
+        $("p."+toShow).show()
+    })
 
-  $(".experience-detail").hide()
-  $(".experience").on("click", function(){
-    $(this).next(".experience-detail").toggle()
-  })
+    $(".experience-detail").hide()
+    $(".experience").on("click", function(){
+        $(this).next(".experience-detail").toggle()
+    })
 })
