a1c10a8a72b6a45ceb0f07c05b85c88520584372
mmazanec22 <melanie.mazanec@gmail.com>
2017-12-24 14:00:14 -0600

    Separate events data; switch nav link oorder

---
+++ javascripts/events.js
@@ -0,0 +1,146 @@
+const eventColors = [
+    '#cc3300',
+    '#ff9933',
+    '#ffcc00',
+    '#9cd615',
+    '#0b8e35',
+    '#39b2aa',
+    '#0066cc',
+    '#0000cc',
+    '#d6149f',
+]
+
+const cvEvents = [
+    {
+        event: 'Private trumpet lesson teacher and freelance musician',
+        daterange: ['06/2005', '06/2016']
+    },
+    {
+        event: 'Interlochen Arts Academy',
+        daterange: ['8/2006', '5/2007']
+    },
+    {
+        event: 'Crossing guard at Interlochen Arts Camp',
+        daterange: ['06/2007', '08/2007']
+    },
+    {
+        event: 'Utrecht Conservatory',
+        daterange: ['8/2007', '6/2008']
+    },
+    {
+        event: 'Sweelinck Orkest',
+        daterange: ['12/2007', '06/2008']
+    },
+    {
+        event: 'Utrecht Blazers Ensemble',
+        daterange: ['01/2008', '06/2008']
+    },
+    {
+        event: 'Cruise ship show band musician (intermittent)',
+        daterange: ['07/2008', '08/2009']
+    },
+    {
+        event: 'Oberlin Conservatory',
+        daterange: ['9/2008', '12/2011']
+    },
+    {
+        event: 'Bicycle down California coast',
+        daterange: ['12/2009', '01/2010']
+    },
+    {
+        event: 'Au Pair in Spain',
+        daterange: ['05/2010', '08/2010']
+    },
+    {
+        event: 'Head cook in dining cooperative',
+        daterange: ['09/2010', '05/2011']
+    },
+    {
+        event: 'Head mechanic in bicycle cooperative',
+        daterange: ['01/2011', '05/2011']
+    },
+    {
+        event: 'Summer school teacher',
+        daterange: ['05/2011', '08/2011']
+    },
+    {
+        event: '4K for Cancer cross-country bicycle trip leg leader',
+        daterange: ['02/2012', '08/2012']
+    },
+    {
+        event: 'Kindergarten co-teacher',
+        daterange: ['10/2012', '03/2013']
+    },
+    {
+        event: 'Bad Girl Ventures entrepreneurship classes',
+        daterange: ['03/2013', '05/2013']
+    },
+    {
+        event: 'Bicycle sales associate',
+        daterange: ['03/2013', '09/2013']
+    },
+    {
+        event: 'Polka band',
+        daterange: ['05/2013', '09/2013']
+    },
+    {
+        event: 'After school music program coordinater and teacher',
+        daterange: ['09/2013', '05/2014']
+    },
+    {
+        event: 'Legacy Initiative of Utah volunteer',
+        daterange: ['01/2014', '06/2014']
+    },
+    {
+        event: 'Special needs tutor',
+        daterange: ['03/2014', '06/2014']
+    },
+    {
+        event: 'CSU Levin College of Urban Affairs MPA',
+        daterange: ['09/2014', '05/2016']
+    },
+    {
+        event: 'CSU Nonprofit Management Certificate',
+        daterange: ['09/2014', '12/2015']
+    },
+    {
+        event: 'CSU brass quintet and El Sistema scholarship',
+        daterange: ['09/2014', '05/2015']
+    },
+    {
+        event: 'Resource development research intern at United Way',
+        daterange: ['10/2014', '05/2015']
+    },
+    {
+        event: 'Graduate assistant',
+        daterange: ['01/2015', '06/2016']
+    },
+    {
+        event: 'Project-based code learning group coordinator',
+        daterange: ['08/2015', '06/2016']
+    },
+    {
+        event: 'CSU Business Analytics Certificate',
+        daterange: ['09/2015', '05/2016']
+    },
+    {
+        event: 'Research Assistant, Cleveland Civic Tech and Data Collaborative',
+        daterange: ['11/2015', '06/2016']
+    },
+    {
+        event: 'Dev Bootcamp',
+        daterange: ['05/2016', '09/2016']
+    },
+    {
+        event: 'Volunteer for Code Platoon',
+        daterange: ['10/2016', '2/2017']
+    },
+    {
+        event: 'Developer at 5th Column',
+        daterange: ['01/2017', '10/2017']
+    },
+    {
+        event: 'Volunteer tech lead at the Difference Engine',
+        daterange: ['09/2017', '10/2017']
+    },
+]
